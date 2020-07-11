d02726bf1cdcd37001d61b008641390045a71445
Anthony Ware <aware91coding@gmail.com>
2020-07-10 22:34:34 -0500

    added the Put but not tested it yet. Need to add the condetions to it.

--- post/post-router.js
+++ post/post-router.js
@@ -109,4 +109,26 @@ router.delete('/:id', (req, res) => {
     })
 })
 
+// Put /posts/:id
+router.put('/:id', (req, res) => {
+  const changes = req.body;
+  Posts
+    .update(req.params.id, changes)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post)
+      } else {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist." 
+        })
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: "The post information could not be modified."
+      })
+    })
+})
+
 module.exports = router;
